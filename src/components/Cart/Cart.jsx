40c153a10ac65c7eec484477474308fd929d0f81
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-03-29 13:19:22 +0600

    added cart component and calculations

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,24 +3,49 @@ import "./Cart.css";
 
 const Cart = ({ cart }) => {
   console.log(cart);
-  //const Cart = (props) => {
-  //const cart = props.cart; (option 1)
-  //const { cart } = props; (option 2)
-  // option 3: write cart directly as parameter inside {}
 
-  // sum of all product's price
-  let total = 0;
+  /*
+  (option 3) write cart directly as parameter inside {}
+  const Cart = (props) => {
+  const cart = props.cart; (option 1)
+  const { cart } = props; (option 2)
+  */
+
+  /*
+  >> sum of all product's price
+  let totalPrice = 0;
   for (const product of cart) {
-    total = total + product.price;
+    totalPrice = totalPrice + product.price;
   }
+
+  >> sum of total shipping
+  let totalShipping = 0;
+  for (const product of cart) {
+    totalShipping = totalShipping + product.shipping;
+  }
+  */
+
+  // Or both total price and total shipping in together(shortcut way)
+  let totalPrice = 0;
+  let totalShipping = 0;
+  for (const product of cart) {
+    totalPrice = totalPrice + product.price;
+    totalShipping = totalShipping + product.shipping;
+  }
+  // tax from total of 5%
+  const tax = (totalPrice * 5) / 100;
+
+  // grand total price
+  const grandTotal = totalPrice + totalShipping + tax;
+
   return (
     <div className="cart-container">
       <h5 className="cart-heading">Order Summary</h5>
       <p>Selected Items: {cart.length}</p>
-      <p>Total Price: {total}</p>
-      <p>Total Shipping:</p>
-      <p>Tax:</p>
-      <h6>Grand Total:</h6>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Total Shipping Charge: ${totalShipping} </p>
+      <p>Tax: ${tax.toFixed(2)} </p>
+      <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
     </div>
   );
 };
