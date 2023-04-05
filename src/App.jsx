895e0c577a5058a237e78895090aca5276f1cad5
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-04-06 01:08:39 +0600

    route setting done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,1 @@
-import "./App.css";
-import Header from "./components/Header/Header";
-import Shop from "./components/Shop/Shop";
 
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <Shop></Shop>
-    </div>
-  );
-}
-
-export default App;
